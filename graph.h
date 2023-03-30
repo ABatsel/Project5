d3b5ac8caf24644db614a0703bcccc8f26756954
tristan333 <tristan.paton33@gmail.com>
2023-03-30 16:44:27 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -4,41 +4,46 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
+#include <climits>
+#include <string>
 
-template <typename K, typename D>
-class Graph {
-public:
-    // Constructor
-    Graph(const std::vector<K>& keys, const std::vector<D>& data, const std::vector<std::vector<K>>& edges);
-
-    // Method to get a pointer to a vertex given a key
-    // Returns nullptr if the vertex does not exist in the graph
-    const D* get(const K& key) const;
-
-    // Method to check if there is a path from vertex u to vertex v
-    bool reachable(const K& u, const K& v) const;
-
-    // Method to print a shortest path from vertex u to vertex v
-    void print_path(const K& u, const K& v) const;
-
-    // Method to get the classification of the edge from vertex u to vertex v
-    std::string edge_class(const K& u, const K& v) const;
-
-    // Method to print the BFS tree starting from vertex s
-    void bfs_tree(const K& s) const;
-
-private:
-    // Define a vertex struct to hold information about each vertex
-    struct Vertex {
-        D data; // vertex data
-        std::unordered_map<K, int> neighbors; // maps adjacent vertex keys to edge weights
-        bool visited; // flag to mark if the vertex has been visited
-        // add any other information you want to store about a vertex here
-    };
-
-    std::unordered_map<K, Vertex> vertices; // maps vertex keys to vertex structs
-
-    // Add any helper methods you need here
+template<typename K, typename D>
+struct Vertex {
+    K key;
+    D data;
+    bool visited;
+    int start_time;
+    int finish_time;
+    int distance;
+    Vertex<K, D>* parent;
+    Vertex<K, D>* next;
+    std::vector<Vertex<K, D>*> adj_list;
+    Vertex(K key, D data)
+        : key(key), data(data), visited(false), start_time(0), finish_time(0), distance(INT_MAX), parent(nullptr), next(nullptr) {}
 };
 
-#endif
+template<typename K, typename D>
+class Graph {
+public:
+    Graph() {}
+    Graph(const std::vector<K>& keys, const std::vector<D>& data, const std::vector<std::vector<K>>& edges);
+    ~Graph() {
+        for (auto& pair : vertices) {
+            delete pair.second;
+        }
+    }
+    Vertex<K,D>* get(K key);
+    bool reachable(const K& u, const K& v) const;
+    void print_path(const K& start_key, const K& end_key);
+    std::string edge_class(K u, K v);
+    void bfs_tree(K s);
+
+private:
+    std::unordered_map<K, Vertex<K, D>*> vertices;
+    bool dfs(Vertex<K, D>* start, K end);
+    void dfs_helper(Vertex<K, D>* start, int& time);
+    void bfs(K start_key);
+};
+
+#include "graph.cpp"
+#endif /* GRAPH_H */
