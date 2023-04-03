e25dc452ade84089beccc23436d355a4288f8feb
RachelZqZeng <122831269+RachelZqZeng@users.noreply.github.com>
2023-04-03 19:49:47 -0400

    new constructor

--- graph.h
+++ graph.h
@@ -1,49 +1,37 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <queue>
 #include <climits>
 #include <string>
+using namespace std;
 
-template<typename K, typename D>
-struct Vertex {
-    K key;
-    D data;
-    bool visited;
-    int start_time;
-    int finish_time;
-    int distance;
-    Vertex<K, D>* parent;
-    Vertex<K, D>* next;
-    std::vector<Vertex<K, D>*> adj_list;
-    Vertex(K key, D data)
-        : key(key), data(data), visited(false), start_time(0), finish_time(0), distance(INT_MAX), parent(nullptr), next(nullptr) {}
-};
-
-template<typename K, typename D>
+template<typename D, typename K>
 class Graph {
-public:
-    Graph() {}
-    Graph(const std::vector<K>& keys, const std::vector<D>& data, const std::vector<std::vector<K>>& edges);
-    ~Graph() {
-        for (auto& pair : vertices) {
-            delete pair.second;
-        }
-    }
-    Vertex<K,D>* get(K key);
-    bool reachable(const K& u, const K& v) const;
-    void print_path(const K& start_key, const K& end_key);
-    std::string edge_class(K u, K v);
-    void bfs_tree(K s);
+public:                      
+                Graph();
+                Graph(vector<D> data, vector<K> key, vector<vector<K>> edges);
+                ~Graph();
+unordered_map<K, Vertex>*         get(K key);
+bool            reachable(const K& u, const K& v) const;
+void            print_path(const K& start_key, const K& end_key);
+std::string     edge_class(K u, K v);
+void            bfs_tree(K s);
+void            bfs(K start_key);
 
+struct Vertex{ //node in graph 
+    K           key;
+    D           data; 
+    string      color;
+    int         distance;
+    list <D, K> adj_list;
+    };
 private:
-    std::unordered_map<K, Vertex<K, D>*> vertices;
-    bool dfs(Vertex<K, D>* start, K end);
-    void dfs_helper(Vertex<K, D>* start, int& time);
-    void bfs(K start_key);
+unordered_map<K, Vertex> vertices; //key -> vertex
+
 };
 
-#include "graph.cpp"
-#endif /* GRAPH_H */
+#endif
